f4f9d2bc51a1bad1b159e099d89e8391882920ae
Jamshid <qq@qq.com>
2016-08-03 19:39:07 -0700

    Fixing format function for ip address.

--- src/Blockchain/UDP.hs
+++ src/Blockchain/UDP.hs
@@ -86,7 +86,18 @@ instance Format NodeDiscoveryPacket where
 data IAddr = IPV4Addr HostAddress | IPV6Addr HostAddress6 deriving (Show, Read, Eq)
 
 instance Format IAddr where
-    format (IPV4Addr x) = show (fromIntegral x::Word8) ++ "." ++ show (fromIntegral $ x `shiftR` 8::Word8) ++ "." ++ show (fromIntegral $ x `shiftR` 16::Word8) ++ "." ++ show (fromIntegral $ x `shiftR` 24::Word8)
+{-    format (IPV4Addr x) =
+      show (fromIntegral x::Word8) ++ "." ++
+      show (fromIntegral $ x `shiftR` 8::Word8) ++ "." ++
+      show (fromIntegral $ x `shiftR` 16::Word8) ++ "." ++
+      show (fromIntegral $ x `shiftR` 24::Word8) -}
+      
+    format (IPV4Addr x) =
+      show (fromIntegral $ x `shiftR` 24::Word8) ++ "." ++
+      show (fromIntegral $ x `shiftR` 16::Word8) ++ "." ++
+      show (fromIntegral $ x `shiftR` 8::Word8) ++ "." ++
+      show (fromIntegral x::Word8)
+      
     format (IPV6Addr (v1, v2, v3, v4)) =
         showHex (fromIntegral $ v4 `shiftR` 16::Word16) "" ++ ":" ++
         showHex (fromIntegral v4::Word16) "" ++ ":" ++
