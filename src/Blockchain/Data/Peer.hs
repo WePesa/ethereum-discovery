8aa064b5ba151c23ff4d1d39bfe9112e539562d5
Jamshid <qq@qq.com>
2016-08-07 14:19:50 -0700

    Adding tool to parse enodes, and add them to the peer database.  I am then using this to insert the bootnodes.

--- src/Blockchain/Data/Peer.hs
+++ src/Blockchain/Data/Peer.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE QuasiQuotes                #-}
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
@@ -8,28 +9,58 @@
 module Blockchain.Data.Peer where
 
 import Crypto.Types.PubKey.ECC
-import Data.Text
+import qualified Data.Text as T
 import Data.Time
+import Data.Time.Clock.POSIX
 import Database.Persist.TH
+import Text.Regex.PCRE
+
 
 import Blockchain.Data.PersistTypes ()
+import Blockchain.Data.PubKey
 import Blockchain.MiscJSON ()
 import Blockchain.SHA
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 PPeer 
     pubkey Point Maybe
-    ip Text
+    ip T.Text
     port Int
     numSessions Int
-    lastMsg Text
+    lastMsg T.Text
     lastMsgTime UTCTime
     enableTime UTCTime
     lastTotalDifficulty Integer
     lastBestBlockHash SHA
-    version Text
+    version T.Text
     deriving Show Read Eq
 |]
+
+jamshidBirth::UTCTime
+jamshidBirth = posixSecondsToUTCTime 0
+
+createPeer::String->PPeer
+createPeer peerString = 
+  PPeer {
+    pPeerPubkey = Just $ stringToPoint pubKey,
+    pPeerIp = T.pack ip,
+    pPeerPort = port,
+    pPeerNumSessions = 0,
+    pPeerLastTotalDifficulty = 0,
+    pPeerLastMsg  = T.pack "msg",
+    pPeerLastMsgTime = jamshidBirth,
+    pPeerEnableTime = jamshidBirth,
+    pPeerLastBestBlockHash = SHA 0,
+    pPeerVersion = T.pack "61" -- fix
+    }
+  where
+    (pubKey, ip, port) = parseEnode peerString
+
+parseEnode::String->(String, String, Int)
+parseEnode enode =
+  case (enode =~ ("enode://([a-f0-9]{128})@(\\d+\\.\\d+\\.\\d+\\.\\d+):(\\d+)"::String))::(String, String, String, [String]) of
+    ("", _, "", [pubKey, ip, port]) -> (pubKey, ip, read port)
+    _ -> error $ "malformed enode: " ++ enode
 
 addPeer'::PPeer->IO ()
 addPeer' = undefined
